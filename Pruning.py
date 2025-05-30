133c9c2caa013dd4dbc10507f16ddb37c251f175
scox3 <scox3@trinity.edu>
2025-05-30 14:04:42 -0500

    Pruned the files to needed columns and updated pruning files

--- Pruning.py
+++ Pruning.py
@@ -1,5 +1,14 @@
-inputLines = open("datafiles/LFData.csv",'r',encoding='utf-8').readlines()
-prunedLF = open('datafiles/LFData_Pruned.csv','w+')
-for val in inputLines:
-  broken = val.split('\t')
-  prunedLF.write(broken[0] + " " + broken[21]+ " " + broken[22] + " " + broken[29]+'\n')
+def prune(inputFile, outputFile):
+    inputLines = open(inputFile,'r',encoding='utf-8').readlines()
+    prunedLF = open(outputFile,'w+',encoding='utf-8')
+    for val in inputLines:
+        broken = val.split('\t')
+        prunedLF.write(broken[0] + " " + broken[21]+ " " + broken[22] + " " + broken[29]+'\n')
+
+prune('datafiles/BW Data.csv','datafiles/BWData_Pruned.csv')
+prune('datafiles/Grape Data.csv','datafiles/GrData_Pruned.csv')
+prune('datafiles/Hops Data.csv','datafiles/HpData_Pruned.csv')
+prune('datafiles/RM Data.csv','datafiles/RMData_Pruned.csv')
+prune('datafiles/SM Data.csv','datafiles/SMData_Pruned.csv')
+prune('datafiles/TOH Data.csv','datafiles/TOHData_Pruned.csv')
+prune('datafiles/LF Data.csv','datafiles/LFData_Pruned.csv')
