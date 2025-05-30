928fc9039165d5fdc665f22237daef90e69764c6
scox3 <scox3@trinity.edu>
2025-05-30 13:22:39 -0500

    pruned testing

--- Pruning.py
+++ Pruning.py
@@ -1,5 +1,5 @@
-inputLines = open("LFData.csv",'r').readLines()
-prunedLF = open('LFData_Pruned.csv','w+')
+inputLines = open("datafiles/LFData.csv",'r',encoding='utf-8').readlines()
+prunedLF = open('datafiles/LFData_Pruned.csv','w+')
 for val in inputLines:
-  broken = val.split('\s')
-  prunedLF.write(broken[0] + " " + broken[24] + broken[29]+'\n')
+  broken = val.split('\t')
+  prunedLF.write(broken[0] + " " + broken[21]+ " " + broken[22] + " " + broken[29]+'\n')
