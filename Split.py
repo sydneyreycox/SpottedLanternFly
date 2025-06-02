e4eaca38e45ce5564d7a640b6064589bbe72956a
scox3 <scox3@trinity.edu>
2025-06-02 13:56:04 -0400

    Split the files and preparing to start attempting to map, alpha shape next

--- Split.py
+++ Split.py
@@ -1,46 +1,47 @@
-def split(inputFile, outputFile):
+from os import mkdir
+
+def split(inputFile, outputPath):
     inputLines = open(inputFile,'r',encoding='utf-8').readlines()
-    split2015 = open(outputFile,'w+',encoding='utf-8')
-    split2016 = open(outputFile,'w+',encoding='utf-8')
-    split2017 = open(outputFile,'w+',encoding='utf-8')
-    split2018 = open(outputFile,'w+',encoding='utf-8')
-    split2019 = open(outputFile,'w+',encoding='utf-8')
-    split2020 = open(outputFile,'w+',encoding='utf-8')
-    split2021 = open(outputFile,'w+',encoding='utf-8')
-    split2022 = open(outputFile,'w+',encoding='utf-8')
-    split2023 = open(outputFile,'w+',encoding='utf-8')
-    split2024 = open(outputFile,'w+',encoding='utf-8')
-    split2025 = open(outputFile,'w+',encoding='utf-8')
+    years = []
+    for i in range(2015,2025):
+        outputName = outputPath+'/'+inputFile.split('/')[1].replace('.csv','')+str(i)+".csv"
+        years.append(open(outputName,'w+',encoding='utf-8'))
+    header = inputLines.pop(0)
+    for y in years:
+        y.write(header)
+
 
     for val in inputLines:
-        broken = val.split('\t')
-        if(broken[29].startswith("2015")): 
-            split2015.write(broken[0] + "," + broken[21]+ "," + broken[22] + "," + broken[29]+'\n')
-        elif(broken[29].startswith("2016")): 
-            split2016.write(broken[0] + "," + broken[21]+ "," + broken[22] + "," + broken[29]+'\n')
-        elif(broken[29].startswith("2017")): 
-            split2017.write(broken[0] + "," + broken[21]+ "," + broken[22] + "," + broken[29]+'\n')
-        elif(broken[29].startswith("2018")): 
-            split2018.write(broken[0] + "," + broken[21]+ "," + broken[22] + "," + broken[29]+'\n')
-        elif(broken[29].startswith("2019")): 
-            split2019.write(broken[0] + "," + broken[21]+ "," + broken[22] + "," + broken[29]+'\n')
-        elif(broken[29].startswith("2020")): 
-            split2020.write(broken[0] + "," + broken[21]+ "," + broken[22] + "," + broken[29]+'\n')
-        elif(broken[29].startswith("2021")): 
-            split2021.write(broken[0] + "," + broken[21]+ "," + broken[22] + "," + broken[29]+'\n')
-        elif(broken[29].startswith("2022")): 
-            split2022.write(broken[0] + "," + broken[21]+ "," + broken[22] + "," + broken[29]+'\n')
-        elif(broken[29].startswith("2023")): 
-            split2023.write(broken[0] + "," + broken[21]+ "," + broken[22] + "," + broken[29]+'\n')
-        elif(broken[29].startswith("2024")): 
-            split2024.write(broken[0] + "," + broken[21]+ "," + broken[22] + "," + broken[29]+'\n')
-        elif(broken[29].startswith("2025")): 
-            split2025.write(broken[0] + "," + broken[21]+ "," + broken[22] + "," + broken[29]+'\n')
-            
-split('datafiles/BWData_Pruned.csv','datafiles/BWData_Split.csv')
-split('datafiles/GrData_Pruned.csv','datafiles/GrData_Split.csv')
-split('datafiles/HpData_Pruned.csv','datafiles/HpData_Split.csv')
-split('datafiles/RMData_Pruned.csv','datafiles/RMData_Split.csv')
-split('datafiles/SMData_Pruned.csv','datafiles/SMData_Split.csv')
-split('datafiles/TOHData_Pruned.csv','datafiles/TOHData_Split.csv')
-split('datafiles/LFData_Pruned.csv','datafiles/LFData_Split.csv')
+        broken = val.split(',')
+        if(broken[3].startswith("2015")): 
+            years[0].write(broken[0] + "," + broken[1]+ "," + broken[2] + "," + broken[3])
+        elif(broken[3].startswith("2016")): 
+            years[1].write(broken[0] + "," + broken[1]+ "," + broken[2] + "," + broken[3])
+        elif(broken[3].startswith("2017")): 
+            years[2].write(broken[0] + "," + broken[1]+ "," + broken[2] + "," + broken[3])
+        elif(broken[3].startswith("2018")): 
+            years[3].write(broken[0] + "," + broken[1]+ "," + broken[2] + "," + broken[3])
+        elif(broken[3].startswith("2019")): 
+            years[4].write(broken[0] + "," + broken[1]+ "," + broken[2] + "," + broken[3])
+        elif(broken[3].startswith("2020")): 
+            years[5].write(broken[0] + "," + broken[1]+ "," + broken[2] + "," + broken[3])
+        elif(broken[3].startswith("2021")): 
+            years[6].write(broken[0] + "," + broken[1]+ "," + broken[2] + "," + broken[3])
+        elif(broken[3].startswith("2022")): 
+            years[7].write(broken[0] + "," + broken[1]+ "," + broken[2] + "," + broken[3])
+        elif(broken[3].startswith("2023")): 
+            years[8].write(broken[0] + "," + broken[1]+ "," + broken[2] + "," + broken[3])
+        elif(broken[3].startswith("2024")): 
+            years[9].write(broken[0] + "," + broken[1]+ "," + broken[2] + "," + broken[3])
+
+def main():
+    tags = ['datafiles/BWData_Pruned.csv','datafiles/GrData_Pruned.csv','datafiles/HpData_Pruned.csv',\
+        'datafiles/RMData_Pruned.csv','datafiles/SMData_Pruned.csv','datafiles/TOHData_Pruned.csv',\
+        'datafiles/LFData_Pruned.csv']
+    locs = ['datefiles/BWDataSplit','datefiles/GrDataSplit','datefiles/HpDataSplit','datefiles/RMDataSplit',\
+        'datefiles/SMDataSplit','datefiles/TOHDataSplit','datefiles/LFDataSplit']
+    for l in locs:
+        mkdir(l)
+    for t,l in zip(tags,locs):
+        split(t,l)
+main() 
