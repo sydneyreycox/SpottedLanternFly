e7390eafbc167119a669ff727893067356d3f275
Hales124 <helliot1@students.towson.edu>
2025-06-03 21:15:28 -0400

    FInished the original CordTally.py, ouutput is [column, row, how many points], extra comments that need to be erased and code must be commented still

--- CordTally.py
+++ CordTally.py
@@ -12,31 +12,41 @@ for line in file:
 B = [-125.0, -69.0, 25.0, 50.0]
 la1 = B[0]
 la2 = B[1]
-#lo1 = B[2]
-lo2 = B[3]
+lo2 = B[2]
 r1 = len(truncData)
 r2 = 0
-tally = 0
+tally = []
+index = 0
+
 
 while la1 < la2:
     #print('la1:',la1)
-    lo1 = B[2]
-    while lo1 < lo2:
+    lo1 = B[3]
+    while lo1 > lo2:
         t1 = 0
         #print('\n',truncData[t1][0], truncData[t1][1])
         #print('lo1:',lo1)
+        indexr = 0
         while t1 < r1:
             #print('t1',t1)
+            present = 0
             if(truncData[t1][0] == la1 and truncData[t1][1] == lo1):
-                tally += 1  
+                #present += 1
+                found = False
+                for pair in tally:
+                    if pair[0] == index:
+                        pair[1] += 1 
+                        found = True
+                        break
+                if not found:
+                    tally.append([index,indexr, 1])
+                #tally.append([index,present])  
                 #print('\n',truncData[t1][0], truncData[t1][1])
             t1 += 1
-        lo1 = round(lo1 + 0.1, 1)
+        lo1 = round(lo1 - 0.1, 1)
+        indexr += 1
+    index += 1
     la1 = round(la1 + 0.1, 1)
+    #print(index)
 
 print(tally)
-
-#for la in range(B[0], B[1]):
- #   print(la)
-   # for lo in range(B[2], B[3]):
-       # if(truncData[lo][la]):
