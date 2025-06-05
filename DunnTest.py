eacf92680d6789be1e5d4d8909d310385da8624f
Hales124 <helliot1@students.towson.edu>
2025-06-05 01:29:02 -0400

    Fixed? The Kruskal-Wallis and Finished the Dunn Test files, they should output correctly now. The Dunn test results are interesting to try and interpret.

--- DunnTest.py
+++ DunnTest.py
@@ -1,34 +1,24 @@
 import scikit_posthocs as sp
 import pandas as pd
+import numpy as np
 
 def populate(path):
-    file = open(path,'r',encoding = 'utf-8').read()
-    num = []
-    numin = [int(n) for n in file.split(',')]
-    num.append(numin)
+    num = list(map(float,(open(path,'r',encoding = 'utf-8').read().split(',')[1:])))
     return num
 
-group1 = populate()
-group2 = populate()
-group3 = populate()
-group4 = populate()
-group5 = populate()
-group6 = populate()
-group7 = populate()
-group8 = populate()
+group1 = populate('nndata/nndata_AS.csv')
+group2 = populate('nndata/nndata_BW.csv')
+group3 = populate('nndata/nndata_RM.csv')
+group4 = populate('nndata/nndata_SM.csv')
+group5 = populate('nndata/nndata_TOH.csv')
 
-data = group1 + group2 + group3 + group4 + group5 + group6 + group7 + group8
+data = group1 + group2 + group3 + group4 + group5
 
-groups = (     
-['group1'] * len(group1)+
-['group2'] * len(group2)+
-['group3'] * len(group3)+
-['group4'] * len(group4)+
-['group5'] * len(group5)+
-['group6'] * len(group6)+
-['group7'] * len(group7)+
-['group8'] * len(group8)
-)
+groups = (['AS'] * len(group1) +
+          ['BW'] * len(group2) +
+          ['RM'] * len(group3) +
+          ['SM'] * len(group4) +
+          ['TOH'] * len(group5))
 
 
  
@@ -36,6 +26,6 @@ df = pd.DataFrame({'data': data, 'group': groups})
 
 #dunnData = [df[n].values for n in df.columns]
 
-dunnResult = sp.posthoc_dunn(df, val_col='data', group_col='group')
+dunnResult = sp.posthoc_dunn(df, val_col='data', group_col='group', p_adjust='bonferroni')
 
 print(dunnResult)
--- KruskWall.py
+++ KruskWall.py
@@ -1,22 +1,35 @@
 from scipy.stats import kruskal
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
 
 def populate(path):
-    file = open(path,'r',encoding = 'utf-8').read()
-    num = []
-    numin = [int(n) for n in file.split(',')]
-    num.append(numin)
+    num = list(map(float,(open(path,'r',encoding = 'utf-8').read().split(',')[1:])))
     return num
         
-group1 = populate()
-group2 = populate()
-group3 = populate()
-group4 = populate()
-group5 = populate()
-group6 = populate()
-group7 = populate()
-group8 = populate()
+group1 = populate('nndata/nndata_AS.csv')
+group2 = populate('nndata/nndata_BW.csv')
+group3 = populate('nndata/nndata_RM.csv')
+group4 = populate('nndata/nndata_SM.csv')
+group5 = populate('nndata/nndata_TOH.csv')
 
-statistic, pvalue = kruskal(group1, group2, group3, group4, group5, group6, group7, group8)
+data = group1 + group2 + group3 + group4 + group5
+groups = (['AS'] * len(group1) +
+          ['BW'] * len(group2) +
+          ['RM'] * len(group3) +
+          ['SM'] * len(group4) +
+          ['TOH'] * len(group5))
 
-print(statistic)
-print(pvalue)
+df = pd.DataFrame({'value': data, 'group': groups})
+
+statistic, pvalue = kruskal(group1, group2, group3, group4, group5)
+
+print(f'Kruskal-Wallis Test\nH = {statistic:.3f}, p = {pvalue:.15e}') #p-value is so small is is not registered through python(practically zero)/
+                                                                    #it is too small for python to register, must to Dunn's test
+
+sns.boxplot(x = 'group', y = 'value', data = df, showfliers = False)#data has a lot of outliers, maybe we should look into a different visualization method.
+plt.title("Kruskal-Wallis Test")
+plt.xlabel("Tree")
+plt.ylabel("Placeholder")
+plt.tight_layout()
+plt.show()
